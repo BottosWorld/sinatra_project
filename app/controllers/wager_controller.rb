6887bc9c48add2c51cef80b15ea2ec431eb9b245
botto54 <bryanlbottorff@gmail.com>
2020-08-08 05:24:49 -0400

    create edit wager view and add form content, add get and patch actions in wager controller to get wager attributes from current user and enable editing, post updated wager from edit wager view to live bets view, change wording in homepage and live bets view, add wager id label to each wager user has placed in live bets view, add notes throughout

--- app/controllers/wager_controller.rb
+++ app/controllers/wager_controller.rb
@@ -26,7 +26,7 @@ class WagerController < ApplicationController
         if @pendingbet.save
             redirect '/live_bets'
         else
-            erb :'/user/place_wager'
+            redirect '/user/place_wager'
         end
         # creates a new wager from the form in place_wager view and assigns to the user who is logged in
         # saves that wager
@@ -39,8 +39,37 @@ class WagerController < ApplicationController
         # gets a list of wagers with attributes from live_bets view of the current user
     end
 
+    get '/live_bets/:id' do
+        set_wager
+        if current_user == @wager.user
+            erb :'/user/edit_wager'
+            # gets wager attributes from list of live bets view for current user and allows user to edit that wager
+        else
+            redirect '/'
+        end
+    end
+
+    patch '/live_bets/:id' do
+        set_wager
+        if current_user == @wager.user && @wager.update(
+            bet: params[:wager][:bet],
+            amount: params[:wager][:amount],
+        )
+            redirect "/live_bets"
+            # {@wager.id}
+        else
+            erb :'/user/edit_wager'
+        end
+    end
+
+
     # CRUD controller
     # Bets get pushed to active bets page where user can view all bets
     # User should also be able to edit and delete their own bets
+
+    private
+    def set_wager
+      @wager = Wager.find_by_id(params[:id])
+    end
 
 end
